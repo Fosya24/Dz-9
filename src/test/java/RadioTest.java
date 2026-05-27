f79aa03a7d04dd3548b316b7031ee64ad6bf7744
Fosya <napka1121@mail.ru>
2026-05-27 14:20:52 +0700

    изменение радио добавлены тесты

--- src/test/java/RadioTest.java
+++ src/test/java/RadioTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import ru.netology.javaqa.Radio;
@@ -19,6 +20,25 @@ public class RadioTest {
             "12, 9"
     })
     public void setStationNumberTest(int stationNumber, int expected) {
+        radio.setStationNumber(stationNumber);
+        int actual = radio.getStationNumber();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "5, 5",
+            "-1, 0",
+            "0, 0",
+            "1, 1",
+            "4, 4",
+            "6, 5",
+            "-5, 0",
+            "12, 5"
+    })
+    public void setStationNumberWithCountStationTest(int stationNumber, int expected) {
+        Radio radio = new Radio(6);
         radio.setStationNumber(stationNumber);
         int actual = radio.getStationNumber();
 
@@ -62,6 +82,23 @@ public class RadioTest {
 
     @ParameterizedTest
     @CsvSource({
+            "0, 1",
+            "1, 2",
+            "5, 6",
+            "8, 0",
+            "6, 0"
+    })
+    void nextWithCountStationTest(int stationNumber, int expected) {
+        Radio radio = new Radio(7);
+        radio.setStationNumber(stationNumber);
+        radio.next();
+        int actual = radio.getStationNumber();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
             "0, 9",
             "1, 0",
             "5, 4",
@@ -69,6 +106,22 @@ public class RadioTest {
             "9, 8"
     })
     void prevTest(int stationNumber, int expected) {
+        radio.setStationNumber(stationNumber);
+        radio.prev();
+        int actual = radio.getStationNumber();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "0, 6",
+            "1, 0",
+            "5, 4",
+            "6, 5",
+    })
+    void prevWithCountStationTest(int stationNumber, int expected) {
+        Radio radio = new Radio(7);
         radio.setStationNumber(stationNumber);
         radio.prev();
         int actual = radio.getStationNumber();
@@ -112,5 +165,55 @@ public class RadioTest {
         int actual = radio.getCurrentVolume();
 
         Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void nextWithInvalidCountStationTest() {
+        Radio radio = new Radio(-2);
+        radio.setStationNumber(5);
+        radio.next();
+        int actual = radio.getStationNumber();
+
+        Assertions.assertEquals(6, actual);
+    }
+
+    @Test
+    void nextWithZeroCountStationTest() {
+        Radio radio = new Radio(0);
+        radio.setStationNumber(5);
+        radio.next();
+        int actual = radio.getStationNumber();
+
+        Assertions.assertEquals(6, actual);
+    }
+
+    @Test
+    void nextWithZeroCountStationInSetterTest() {
+        radio.setCountStation(0);
+        radio.setStationNumber(5);
+        radio.next();
+        int actual = radio.getStationNumber();
+
+        Assertions.assertEquals(6, actual);
+    }
+
+    @Test
+    void nextWithInvalidCountStationInSetterTest() {
+        radio.setCountStation(-1);
+        radio.setStationNumber(5);
+        radio.next();
+        int actual = radio.getStationNumber();
+
+        Assertions.assertEquals(6, actual);
+    }
+
+    @Test
+    void nextWithCountStationInSetterTest() {
+        radio.setCountStation(1);
+        radio.setStationNumber(0);
+        radio.next();
+        int actual = radio.getStationNumber();
+
+        Assertions.assertEquals(0, actual);
     }
 }
